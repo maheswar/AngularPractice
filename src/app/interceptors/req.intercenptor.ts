256a5d72ac49640e3db23380a71c8acbce8e6a1d
maheswar <maheswarreddy4@gmail.com>
2020-02-25 00:02:52 +0530

    auto login

--- src/app/interceptors/req.intercenptor.ts
+++ src/app/interceptors/req.intercenptor.ts
@@ -1,20 +1,20 @@
-import { HttpEvent,HttpRequest,HttpHandler,HttpInterceptor, HttpParams } from "@angular/common/http";
-import {Observable, throwError} from "rxjs"
-import { AuthService } from "../auth/auth.service";
-import { Injectable } from "@angular/core";
+import { HttpEvent, HttpRequest, HttpHandler, HttpInterceptor, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { Injectable } from '@angular/core';
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root'
 })
-export class RequestInterceptors implements HttpInterceptor{
-    constructor(private auth:AuthService){
+export class RequestInterceptors implements HttpInterceptor {
+    constructor(private auth: AuthService) {
 
     }
-    intercept(req:HttpRequest<any>, next:HttpHandler):Observable<HttpEvent<any>> {
-        if(!this.auth.loginKey||this.auth.loginKey==""){
-            return throwError("Your session has time out")
-       }
-       let modifiedReq=req.clone({params:new HttpParams().set('auth',this.auth.loginKey)})
-       modifiedReq.headers.append("key","213654897")
-       return next.handle(modifiedReq)      
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (!this.auth.loginKey || this.auth.loginKey === '') {
+            return throwError('Your session has time out');
+        }
+        const modifiedReq = req.clone({ params: new HttpParams().set('auth', this.auth.loginKey) });
+        modifiedReq.headers.append('key', '213654897');
+        return next.handle(modifiedReq);
     }
 }
--- src/app/usercreate/userservice.ts
+++ src/app/usercreate/userservice.ts
@@ -1,72 +1,76 @@
 import { Observable, Subject } from 'rxjs';
 
-interface User{
-    UserName:string;
-    Password:string;
-    Email:string;
-    Mobile:string;
-    Token:string;
-    ExpiryDate:Date;
+interface User {
+    UserName: string;
+    Password: string;
+    Email: string;
+    Mobile: string;
+    Token: string;
+    ExpiryDate: Date;
 }
-export class UserService{
-    lstUsers:User[]=[];
-    loginTime:number=10;
-    logOut:Subject<boolean>=new Subject<boolean>();
-    logInSub:Subject<User>=new Subject<User>();
+export class UserService {
+    lstUsers: User[] = [{
+        UserName: 'tester', Password: '1234',
+        ExpiryDate: new Date('02/02/2030'), Token: this.getRandomToken()
+    } as User];
+    loginTime = 10;
+    logOut: Subject<boolean> = new Subject<boolean>();
+    logInSub: Subject<User> = new Subject<User>();
 
-    addUser(username,pswd){
-        let token=this.getRandomToken()
-        this.lstUsers.push({UserName:username,Password:pswd,Token:token} as User);
+    addUser(username, pswd) {
+        const token = this.getRandomToken();
+        this.lstUsers.push({ UserName: username, Password: pswd, Token: token } as User);
     }
 
-    logout(token:string){
-       this.lstUsers= this.lstUsers.filter(e=>e.Token!=token)
-       localStorage.removeItem('userData');
-       this.logOut.next(true);
+    logout(token: string) {
+        this.lstUsers = this.lstUsers.filter(e => e.Token != token)
+        localStorage.removeItem('userData');
+        this.logOut.next(true);
     }
 
-    autoLogin(){
-        let encUserInfo=localStorage.getItem('userData');
-        if(!!encUserInfo){
-          let userInfo = JSON.parse(window.atob(encUserInfo)) as User;
-          if(this.isValidUser(userInfo)){
-            this.logInSub.next(userInfo);
-          }else{
-              this.logout('');
-          }
+    autoLogin() {
+        const encUserInfo = localStorage.getItem('userData');
+        if (!!encUserInfo) {
+            const userInfo = JSON.parse(window.atob(encUserInfo)) as User;
+            if (this.isValidUser(userInfo)) {
+                this.logInSub.next(userInfo);
+            } else {
+                this.logout('');
+            }
         }
     }
-    
-    isValidUser(userInfo:User){
-        if(new Date()<new Date(userInfo.ExpiryDate)&&userInfo.Token!=""){
+
+    isValidUser(userInfo: User) {
+        if (new Date() < new Date(userInfo.ExpiryDate) && userInfo.Token !== '') {
             return true;
         }
         return false;
     }
 
-    login(username,pswd){
-        var that=this;
-        return new Promise(function(res,rej){
-            setTimeout(()=>{
-                let userInfo= that.lstUsers.filter(e=>e.UserName.toLocaleLowerCase()==username.toLocaleLowerCase() && e.Password==pswd)
-                if(userInfo.length>0){
-                    let userData=userInfo[0];
-                    var d1 = new Date (),
-                    d2 = new Date ( d1 );
-                    d2.setMinutes ( d1.getMinutes() + that.loginTime );
-                    userData.ExpiryDate=d2;
+    login(username, pswd) {
+        const that = this;
+        return new Promise(function (res, rej) {
+            setTimeout(() => {
+                const userInfo = that.lstUsers.filter(e => e.UserName.toLocaleLowerCase() === username.toLocaleLowerCase()
+                    && e.Password === pswd);
+                if (userInfo.length > 0) {
+                    const userData = userInfo[0];
+                    const d1 = new Date();
+                    const d2 = new Date(d1);
+                    d2.setMinutes(d1.getMinutes() + that.loginTime);
+                    userData.ExpiryDate = d2;
 
                     that.logInSub.next();
-                    localStorage.setItem('userData',window.btoa(JSON.stringify(userData)));
-                    res(userData.Token);        
-                }else{
-                    rej("Invalid credentials");
+                    localStorage.setItem('userData', window.btoa(JSON.stringify(userData)));
+                    res(userData.Token);
+                } else {
+                    rej('Invalid credentials');
                 }
-            },1500)
-        })
+            }, 1500);
+        });
     }
 
-    getRandomToken():string{
-       return (((Math.random()*1000)+999)*2-89).toString()
+    getRandomToken(): string {
+        return (((Math.random() * 1000) + 999) * 2 - 89).toString()
     }
 }
